c463145dbc5b091ea784dd866df5cfaf59c0294b
sswellington <sswellington@pm.me>
2023-10-12 17:06:17 -0300

    feat: serilog

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# boleto
+# Boleto
+
+API .NET com .NET 6, utilizando Entity Framework como ORM, PostgreSQL como banco de dados e documentação Swagger. A API inclui endpoints para as entidades Boleto e Banco
+
+## Pré-requisitos
+
+Antes de iniciar, certifique-se de que você tenha instalado o seguinte:
+- .NET 6 SDK
+- PostgreSQL
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,8 +1,15 @@
+using Serilog;
+
+namespace Api;
+
 public static class Program
 {
 	private static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
+
+		builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
 		builder.Services.AddControllers();
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
@@ -11,13 +18,17 @@ public static class Program
 
 		if (app.Environment.IsDevelopment())
 		{
+			Log.Information("building the application in Development");
 			app.UseSwagger();
 			app.UseSwaggerUI();
 		}
 
+		app.UseSerilogRequestLogging();
 		app.UseHttpsRedirection();
 		app.UseAuthorization();
 		app.MapControllers();
 		app.Run();
+
+		Log.Information("finished");
 	}
 }
