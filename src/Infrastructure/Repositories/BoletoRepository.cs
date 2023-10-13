5e43ce2659e896688fdee380d1b1f416abd5ac47
sswellington <sswellington@pm.me>
2023-10-13 10:28:17 -0300

    feat: get by id

--- src/Infrastructure/Repositories/BoletoRepository.cs
+++ src/Infrastructure/Repositories/BoletoRepository.cs
@@ -1,10 +1,20 @@
 using Application.Entities;
 using Application.Interfaces.Repositories;
 using Infrastructure.ObjectRelationalMapping.PostgreSql;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
 public sealed class BoletoRepository : BaseRepository<BoletoEntity>, IBoletoRepository
 {
 	public BoletoRepository(BoletoContext boletoContext) : base(boletoContext) { }
+
+	public override async Task<BoletoEntity> GetById(int id)
+	{
+		var result = await _context.Boleto
+			.Include(boleto => boleto.Banco)
+			.SingleOrDefaultAsync(q => q.Id == id)
+			.ConfigureAwait(false);
+		return result ?? new BoletoEntity();
+	}
 }
